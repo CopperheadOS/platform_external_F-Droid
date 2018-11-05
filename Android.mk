bc3f540a9d153412404fea85d5c27497e7059e95
Rashed Abdel-Tawab <rashed@linux.com>
2018-11-05 13:11:40 -0800

    Update to 1.4 and use presigned APK

--- Android.mk
+++ Android.mk
@@ -3,8 +3,11 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := F-Droid
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
-LOCAL_CERTIFICATE := $(DEFAULT_SYSTEM_DEV_CERTIFICATE)
-LOCAL_SRC_FILES := prebuilt/app-release-unsigned.apk
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_SRC_FILES := prebuilt/org.fdroid.fdroid.apk
 
 include $(BUILD_PREBUILT)
